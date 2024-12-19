3dc2193caf5f27f04a0362a9a7bc654e9b8cff82
soumyayalahankaajjegowdagit config --global user.name soumyayalahankaajjegowda <soumya.ajjegowda@gmail.com>
2024-12-19 15:35:44 -0700

    last process

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inputBox = document.getElementById("input-box"); //create variable for elements.
 const listContainer = document.getElementById("list-container");  // create variable for elements.
 console.log(window.innerWidth)
+
 function addTask(){             //define addTask
     if(inputBox.value === ''){       //if condition to check inputvalue
         alert("you must write something!"); //if input box is empty  
@@ -25,15 +26,15 @@ function addTask(){             //define addTask
     listContainer.appendChild(li); // what ever we add in input field that will display in listcontainer.
 }
 
-// listContainer. addEventListener("click", function(e){
-//     if(e.target.tagName === "LI"){            // it will check where we have clicked.
-//         e.target.classList.toggle("checked");
-//         }
-// else if(e.target.tagName === "SPAN"){
-//     e.target.parentElement.remove();   // parent element li will be removed.
-//     saveData();   // call saveData only when a task is remved.
-// }
-// }), 
+listContainer. addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){            // it will check where we have clicked.
+        e.target.classList.toggle("checked");
+        }
+else if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove();   // parent element li will be removed.
+    saveData();   // call saveData only when a task is remved.
+}
+}), 
 
 function saveData(){      //lets create function saveData    //call saveData to save the new data in the browser.
     localStorage.setItem("data", listContainer.innerHTML); // contents in list container will be stored in Data.
