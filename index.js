c35d431f2173e16253271f4de01e14230a4900d4
soumyayalahankaajjegowdagit config --global user.name soumyayalahankaajjegowda <soumya.ajjegowda@gmail.com>
2024-12-19 08:03:57 -0700

    last progress

--- index.js
+++ index.js
@@ -4,18 +4,23 @@ const listContainer = document.getElementById("list-container");  // create vari
 function addTask(){             //define addTask
     if(inputBox.value === ''){       //if condition to check inputvalue
         alert("you must write something!"); //if input box is empty  
-        return; 
-    }      
-        let li = document.createElement("li"); // creating 1 element in document li
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
+    }      else   {
+        let li = document.createElement("li"); // creating 1 element in document li .create method is used.
+       li.innerHTML = inputBox.value;
+        listContainer.appendChild(li); // what ever we add in input field that will display in listcontainer.
 
         let span = document.createElement("span"); //using a font Awesome class for close icon
-        span.classList.add("fas", "fa-times"); //add font Awesome classes
+        span.innerHTML = "\u00d7";
+        //span.classList.add("fas", "fa-times"); //add font Awesome classes
         li.appendChild(span);
+        // listContainer.appendChild(li);
 
+        span.addEventListener("click", function() {
+            li.remove(); // Remove the list item when close icon is clicked
+
+    });
     inputBox.value = "";
-    saveData(); // saveData is called only when newtask is added.
+    //saveData(); // saveData is called only when newtask is added.
 }
 
 listContainer. addEventListener("click", function(e){
@@ -36,3 +41,4 @@ function showTask(){     //it will give all the content stored in the name of Da
 }
 showTask();   // the function ,display tasks on page load.
 
+}
