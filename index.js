276ab4f30ef626574191da8e81ecc97fdcede928
soumyayalahankaajjegowdagit config --global user.name soumyayalahankaajjegowda <soumya.ajjegowda@gmail.com>
2024-12-19 15:16:12 -0700

    last progress

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
 const inputBox = document.getElementById("input-box"); //create variable for elements.
 const listContainer = document.getElementById("list-container");  // create variable for elements.
-
+console.log(window.innerWidth)
 function addTask(){             //define addTask
     if(inputBox.value === ''){       //if condition to check inputvalue
         alert("you must write something!"); //if input box is empty  
     }      else   {
         let li = document.createElement("li"); // creating 1 element in document li .create method is used.
-       li.innerHTML = inputBox.value;
-        listContainer.appendChild(li); // what ever we add in input field that will display in listcontainer.
+       li.textContent = inputBox.value;
+       console.log(li)
+       console.log(listContainer)
+   
 
         let span = document.createElement("span"); //using a font Awesome class for close icon
         span.innerHTML = "\u00d7";
         //span.classList.add("fas", "fa-times"); //add font Awesome classes
-        li.appendChild(span);
-        // listContainer.appendChild(li);
+       // listContainer.appendChild(span);
+         li.appendChild(span);
 
-        span.addEventListener("click", function() {
-            li.remove(); // Remove the list item when close icon is clicked
+        span.addEventListener("click", function(e) {
+            e.target.parentElement.remove(); // Remove the list item when close icon is clicked
 
     });
     inputBox.value = "";
-    //saveData(); // saveData is called only when newtask is added.
+    listContainer.appendChild(li); // what ever we add in input field that will display in listcontainer.
 }
 
-listContainer. addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){            // it will check where we have clicked.
-        e.target.classList.toggle("checked");
-        }
-else if(e.target.tagName === "SPAN"){
-    e.target.parentElement.remove();   // parent element li will be removed.
-    saveData();   // call saveData only when a task is remved.
-}
-}), 
+// listContainer. addEventListener("click", function(e){
+//     if(e.target.tagName === "LI"){            // it will check where we have clicked.
+//         e.target.classList.toggle("checked");
+//         }
+// else if(e.target.tagName === "SPAN"){
+//     e.target.parentElement.remove();   // parent element li will be removed.
+//     saveData();   // call saveData only when a task is remved.
+// }
+// }), 
 
 function saveData(){      //lets create function saveData    //call saveData to save the new data in the browser.
     localStorage.setItem("data", listContainer.innerHTML); // contents in list container will be stored in Data.
@@ -39,6 +41,6 @@ function saveData(){      //lets create function saveData    //call saveData to 
 function showTask(){     //it will give all the content stored in the name of Data.
     listContainer.innerHTML = localStorage.getItem("data");
 }
-showTask();   // the function ,display tasks on page load.
+// showTask();   // the function ,display tasks on page load.
 
 }
