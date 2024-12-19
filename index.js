17542c94375bc3f00ef2e619405b3129e608f5ea
soumyayalahankaajjegowdagit config --global user.name soumyayalahankaajjegowda <soumya.ajjegowda@gmail.com>
2024-12-18 23:59:08 -0700

    last progress

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
+const inputBox = document.getElementById("input-box"); //create variable for elements.
+const listContainer = document.getElementById("list-container");  // create variable for elements.
 
-function addTask(){
-    if(inputBox.value === ''){
-        alert("you must write something!");
+function addTask(){             //define addTask
+    if(inputBox.value === ''){       //if condition to check inputvalue
+        alert("you must write something!");  //if input box is empty
     }
     else{
-        let li = document.createElement("li");
+        let li = document.createElement("li"); // creating 1 element in document li
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
     inputBox.value = "";
 }
 
 listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI")
+    if(e.target.tagName === "LI"){            // it will check where we have clicked.
         e.target.classList.toggle("checked");
+        saveData();
 }
 else if(e.target.tagName === "SPAN"){
-    e.target.parentElement.remove();
+    e.target.parentElement.remove();   // parent element li will be removed.
+    saveData();
+}
 }, false);
 
-function saveData(){
-    localStorage.setItem("data", listContainer.innerHTML);
+function saveData(){      //lets create function saveData    //call saveData to save the new data in the browser.
+    localStorage.setItem("data", listContainer.innerHTML); // contents in list container will be stored in Data.
 }
-function showTask(){
+function showTask(){     //it will give all the content stored in the name of Data.
     listContainer.innerHTML = localStorage.getItem("data");
 }
-showTask();
+showTask();   // the function
 
